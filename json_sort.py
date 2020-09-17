6a8435f63f6314417aefb440197059456143d2b0
yang521 <1483885497@qq.com>
2020-09-17 11:13:05 +0800

    Create json_sort.py

---
+++ json_sort.py
@@ -0,0 +1,26 @@
+import json
+
+
+def dict_sort_by_key(data):
+    data = sorted(data.items(), key=lambda item: item[0])
+    return {i[0]: i[1] for i in data}
+
+
+def json_sort(input_data):
+    if isinstance(input_data, dict):
+        input_data = dict_sort_by_key(input_data)
+        for k in input_data:
+            if isinstance(input_data[k], dict):
+                input_data[k] = dict_sort_by_key(input_data[k])
+                input_data[k] = json_sort(input_data[k])
+            if isinstance(input_data[k], list):
+                input_data[k] = json_sort(input_data[k])
+
+    if isinstance(input_data, list):
+        for i in range(len(input_data)):
+            input_data[i] = json_sort(input_data[i])
+
+    return input_data
+    
+#  功能介绍: 处理json数据对比中字段顺序不一致的问题。
+#  json数据通过此处理再配合Beyond Compare 4 做数据对比，可降低大量的人工成本和时间。
