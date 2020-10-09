2465646f77bbbf437a4b49ad4ae0bbd1d10c36a0
yang521 <1483885497@qq.com>
2020-10-09 17:54:48 +0800

    Create filter_emoji.py

---
+++ filter_emoji.py
@@ -0,0 +1,6 @@
+def filter_emoji(desstr, restr=''):
+    try:
+        co = re.compile(u'[\U00010000-\U0010ffff]')
+    except re.error:
+        co = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
+    return co.sub(restr, desstr)
