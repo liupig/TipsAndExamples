4a91acac02617d96ae008a9fb7c9b7094fbe7c99
yang521 <1483885497@qq.com>
2020-11-06 15:43:24 +0800

    Create data_process.py

---
+++ data_process.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import random
+
+
+def process_text(text, title):
+    def inner_process(text, title):
+        return str(title) + str(text)
+
+    speedup = np.frompyfunc(inner_process, 2, 1)  # 2：  几个参数, 1： 几个返回值
+
+    return speedup(text, title)
+
+
+def process_fee(x):
+    if not x:
+        return None
+    park_fee = ""
+    return park_fee
+
+
+df = pd.read_csv("Train_Data.csv", encoding="utf8")
+df["text"] = process_text(df["text"], df["title"])
+# df["pText"] = df["Fee"].apply(lambda x: x if x else None)
+# df["pFee"] = df["PFee"].apply(process_fee)
+
+df = df.where(df.notnull(), None)
+
+result = df[["text", "negative"]]
+
+result = result.to_dict(orient="records")
+random.shuffle(result)
+
+result_len = len(result)
+head = int(result_len * 0.8)
+pd.DataFrame(result[:head]).to_csv("head.csv", encoding="utf8")
+pd.DataFrame(result[head:]).to_csv("tail.csv", encoding="utf8")
+
+#
+# result.head(int(result_len * 0.8)).to_csv("head.csv", encoding="utf8")
+#
+# result.tail(result_len - head).to_csv("tail.csv", encoding="utf8")
